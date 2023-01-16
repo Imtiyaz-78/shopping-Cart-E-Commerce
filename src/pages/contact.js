c1b5bee48c23408d27f5abfc98a157f27f6e8ac5
Imtiyazdeveloper <imtiyazmd803@gmail.com>
2023-01-16 13:55:24 +0530

    added

---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import contact from "./contact"
+
+export const Contact = () => {
+  return (
+    <div className="contac-us">
+      <h1>Name Md Imtiyaz</h1>
+      <h2>Email: imtiyazmd803@gmail.com</h2>
+      <p>Adress: Noida India</p>
+    </div>
+  )
+};
